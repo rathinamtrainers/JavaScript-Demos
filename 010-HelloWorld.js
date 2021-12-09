62e57bff79e9e623377f749806cecbc44b4e94dd
elango <elango.angannan@hpe.com>
2021-12-09 19:37:06 +0530

    Initial Files

---
+++ 010-HelloWorld.js
@@ -0,0 +1,9 @@
+
+
+let s = "Rathinam Trainers and Consultants Private Limited";
+console.log(s)
+
+console.log(s.substring(9, 16+1));
+console.log(s.toUpperCase())
+console.log(s.toLowerCase())
+
---
+++ UI programming/scripts/27-Functions-in-SeparateFile.js
@@ -0,0 +1,10 @@
+// This script will be executed when this document is loaded
+document.getElementById("title1").innerHTML = "<h1>Java Script Demo</h1>";
+
+function displayText() {
+    document.getElementById('mytext1').innerText='Rathinam Trainers';
+}
+
+function changeTitle() {
+    document.title = "New Title"
+}
